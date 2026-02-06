b1bd9560e7c2fa70e75b08d6d714a14c104e3d50
Santiago Arranz <santiago.arranz.sanz@gmail.com>
2026-02-07 00:29:34 +0100

    Add import unittest

--- tests/test_Lagn.py
+++ tests/test_Lagn.py
@@ -1,13 +1,13 @@
 #python -m unittest tests/test_Lagn.py 
 
-from unittest import TestCase
+import unittest
 import numpy as np
 from numpy.testing import assert_allclose
 
 import gne.gne_const as c
 import gne.gne_Lagn as agn
 
-class TestPredict(TestCase):
+class TestPredict(unittest.TestCase):
     def test_Ledd(self):
         mbh0 = 1e-38; val0 = 1.26
         self.assertAlmostEqual(agn.get_Ledd(mbh0),val0,2)
